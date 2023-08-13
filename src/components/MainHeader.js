3b3324b6e8bb05893335ce3bb698d566b564b993
harshit107 <harshit107.in@gmail.com>
2023-08-13 22:23:43 +0530

    first commit

---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+import gifLogo from '../images/my-cloud-mate-logo.gif'
+const MainHeader = (props) => { 
+
+   return (
+      <div> 
+        <img src={gifLogo} alt="Website logo" />
+
+      </div>
+   )
+}
+export default MainHeader;
